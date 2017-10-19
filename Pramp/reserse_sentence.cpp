54c278a1a00b3cb64417520bc6dc44ab157f1c0c
Fedor Kitashov <fedor.kitashov@phystech.edu>
2017-10-19 15:51:52 -0700

    correct reverse sentence

--- Pramp/reserse_sentence.cpp
+++ Pramp/reserse_sentence.cpp
@@ -6,6 +6,56 @@ using namespace std;
 
 
 
+void Swap(char& i, char& j) {
+  char temp;
+  temp = i;
+  i = j;
+  j = temp;
+  
+}
+
+
+void ReverseWord(vector<char>& arr, int i, int j) {
+  while (i < j) {
+  Swap(arr[i], arr[j]);
+  i++;
+  j--;
+  }
+}
+
+
+  
+vector<char> reverseWords( const vector<char>& a) 
+{
+  int i = 0;
+  int j = 0;
+  vector<char> arr = a;
+  int numberOfSpaces = 0;
+  while (j < arr.size() - 1) {
+    if (arr[j] == ' ') {
+      numberOfSpaces++;
+      ReverseWord(arr, i, j - 1);
+      i = j + 1;
+    }
+    j++;
+  }
+  if (numberOfSpaces > 0) {
+    ReverseWord(arr, i, arr.size() - 1);
+    ReverseWord(arr, 0, arr.size() - 1);
+  }
+  return arr;
+}
+
+int main() {
+  return 0;
+}#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+
+
 
 void PrintVector(const std::vector<char>& v) {
   for (const char& element : v) {
